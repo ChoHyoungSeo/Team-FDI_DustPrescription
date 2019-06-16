69ca836a3a6ef446d71fff5332743c7c0ee91b5d
Minji Kim <wisdomin121@naver.com>
2019-06-16 23:20:32 +0900

    add first page (we should add logo in this page)

---
+++ app/src/main/java/com/cookandroid/registration/First.java
@@ -0,0 +1,13 @@
+package com.cookandroid.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class First extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
