64fcb103c431e13c865a4378dca15aad575aca94
Minji Kim <wisdomin121@naver.com>
2019-06-17 01:00:57 +0900

    add Splash

---
+++ app/src/main/java/com/cookandroid/registration/LoadingActivity.java
@@ -0,0 +1,25 @@
+package com.cookandroid.registration;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoding();
+    }
+
+    private void startLoding(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
--- app/src/main/java/com/cookandroid/registration/Name.java
+++ app/src/main/java/com/cookandroid/registration/Name.java
@@ -1,5 +1,6 @@
 package com.cookandroid.registration;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,7 @@ public class Name extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.name);
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
     }
 }
