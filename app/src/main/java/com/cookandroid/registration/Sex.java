c1f5e96f3305a4539b314d98c5f98fd7ddd2af13
Hyejin00 <svivs137@naver.com>
2019-06-16 23:55:37 +0900

    .

---
+++ app/src/main/java/com/cookandroid/registration/Sex.java
@@ -0,0 +1,13 @@
+package com.cookandroid.registration;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Sex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sex);
+    }
+}
