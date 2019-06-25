d59c762640f06ac677e9f338357c04628403279b
Hyejin00 <svivs137@naver.com>
2019-06-26 01:51:20 +0900

    connection modi

--- app/src/main/java/com/cookandroid/registration/Frg_Quiz.java
+++ app/src/main/java/com/cookandroid/registration/Frg_Quiz.java
@@ -22,7 +22,7 @@ public class Frg_Quiz extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.frg_main, container, false);
+        View v = inflater.inflate(R.layout.activity_quiz, container, false);
 
         date = v.findViewById(R.id.date);
         quiz = v.findViewById(R.id.quiz);
