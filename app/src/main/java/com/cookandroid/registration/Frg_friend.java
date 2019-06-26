e39fc97f817d0555f8028fef64efaef3fdaf6858
ChoHyoungSeo <francesco.johs@gmail.com>
2019-06-26 11:46:13 +0900

    Add: Message

--- app/src/main/java/com/cookandroid/registration/Frg_friend.java
+++ app/src/main/java/com/cookandroid/registration/Frg_friend.java
@@ -7,12 +7,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Frg_friend extends Fragment {
-
+    Button friend01, friend02, friend03, friend04, friend05
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_friend, container, false);
+        View v = inflater.inflate(R.layout.activity_friend, container, false);
+
+        friend01 = v.findViewById(R.id.friend01);
+        friend02 = v.findViewById(R.id.friend02);
+        friend03 = v.findViewById(R.id.friend03);
+        friend04 = v.findViewById(R.id.friend04);
+        friend05 = v.findViewById(R.id.friend05);
+
+        friend01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        return v;
     }
 }
