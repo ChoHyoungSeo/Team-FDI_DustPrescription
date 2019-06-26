d376f7f88b6977827d320a4992144ada8353ee97
uoobin <rladbdms0581@naver.com>
2019-06-26 09:20:18 +0900

    fragment

--- app/src/main/java/com/cookandroid/registration/Frg_Quiz.java
+++ app/src/main/java/com/cookandroid/registration/Frg_Quiz.java
@@ -1,5 +1,6 @@
 package com.cookandroid.registration;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -18,6 +21,8 @@ public class Frg_Quiz extends Fragment {
     Date mDate;
     SimpleDateFormat mFormat = new SimpleDateFormat("mm-dd");
     TextView date, quiz;
+    LinearLayout quizAnsLayout;
+    Button o, x, next;
 
     @Nullable
     @Override
@@ -26,8 +31,34 @@ public class Frg_Quiz extends Fragment {
 
         date = v.findViewById(R.id.date);
         quiz = v.findViewById(R.id.quiz);
+        quizAnsLayout = v.findViewById(R.id.quizAnsLayout);
+        o = v.findViewById(R.id.o);
+        x = v.findViewById(R.id.x);
+        next = v.findViewById(R.id.next);
 
         //quiz.setText();
+
+        o.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quizAnsLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quizAnsLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Quiz2.class);
+                startActivity(intent);
+            }
+        });
         return v;
     }
 
