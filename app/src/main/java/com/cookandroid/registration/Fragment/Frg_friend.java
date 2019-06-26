922e6edcec5c17036f92b9feb36b383aaf79a077
uoobin <rladbdms0581@naver.com>
2019-06-26 23:18:42 +0900

    error fix

--- app/src/main/java/com/cookandroid/registration/Fragment/Frg_friend.java
+++ app/src/main/java/com/cookandroid/registration/Fragment/Frg_friend.java
@@ -1,6 +1,8 @@
 package com.cookandroid.registration.Fragment;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
